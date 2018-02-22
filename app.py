a388121e80b3bc75a150e1dae5e4557cf7c28178
SOUMYA167 <soumyajain1997@gmail.com>
2018-02-22 15:20:57 +0530

    adding app.py

--- app.py
+++ app.py
@@ -28,17 +28,17 @@ def webhook():
     return r
 
 def makeWebhookResult(req):
-    if req.get("result").get("action") != "shipping.cost":
+    if req.get("result").get("action") != "how_are_you":
         return {}
     result = req.get("result")
     parameters = result.get("parameters")
-    zone = parameters.get("shipping-zone")
+    zone = parameters.get("how_r_u")
 
-    cost = {'Europe':100, 'North America':200, 'South America':300, 'Asia':400, 'Africa':500}
+    #cost = {'Europe':100, 'North America':200, 'South America':300, 'Asia':400, 'Africa':500}
 
-    speech = "The cost of shipping to " + zone + " is " + str(cost[zone]) + " euros."
-
-    print("Response:")
+   # speech = "The cost of shipping to " + zone + " is " + str(cost[zone]) + " euros."
+    speech=str(zone)
+    print(zone)
     print(speech)
 
     return {
