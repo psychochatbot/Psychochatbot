7deb41f8eb62f6099160781111b7e013e405f513
psychochatbot <35366584+psychochatbot@users.noreply.github.com>
2018-03-08 12:47:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ def webhook():
     return r
 
 def makeWebhookResult(req):
-    if req.get("result").get("action") == "how_are_you":
+    if req.get("result").get("action") == "how_was_your_day":
       #  return {}
         result = req.get("result")
         parameters = result.get("parameters")
