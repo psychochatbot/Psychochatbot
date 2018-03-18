12c1bc324252d881f355f9012e39ee91498243f9
psychochatbot <35366584+psychochatbot@users.noreply.github.com>
2018-03-18 18:03:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ def webhook():
     res = makeWebhookResult(req)
 
     res = json.dumps(res, indent=4)
-    print(res)
+    print(res) 
     r = make_response(res)
     r.headers['Content-Type'] = 'application/json'
     return r
