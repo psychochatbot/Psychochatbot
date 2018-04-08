fb24200c11418cd0ad9349eb7b2a4089437c5ec7
psychochatbot <35366584+psychochatbot@users.noreply.github.com>
2018-04-08 15:24:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -58,7 +58,9 @@ def webhook():
 
 def dump_value(key,value):
     with io.open('data.json', 'a', encoding='utf8') as outfile:
-        entry[str(key)]=value
+        data_loaded =json.loads(data_file.read())
+        abc=eval(data_loaded)
+        data_loaded[key]=value
         str_=json.dumps(entry,ensure_ascii=False)
         outfile.write((str_))
 
